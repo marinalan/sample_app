d436fb53801a03b52737e9ed394cf37ef804ea55
Marina Landisberg <mlandisberg@gmail.com>
2014-09-19 21:30:25 -0700

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -47,6 +47,17 @@ describe "Static Pages" do
         end
       end
 
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
       it "should show microposts count" do
         expect(page).to have_selector("div.row aside.span4 section span", text: "micropost".pluralize(user.feed.count))
       end
